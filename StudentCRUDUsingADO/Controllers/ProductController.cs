aee09e08819c4d9c19414c2ad4d26ceb05f43827
madhujogdand <gore.madhu03@gmail.com>
2024-05-18 15:34:06 +0530

    Commit Changes

---
+++ StudentCRUDUsingADO/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentCRUDUsingADO.Models;
+
+namespace StudentCRUDUsingADO.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL productdal;
+        private readonly IConfiguration configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            productdal = new ProductDAL(this.configuration);
+        }
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var model=productdal.GetProducts();
+            return View(model);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            var product=productdal.GetProductById(id);
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                int result = productdal.AddProduct(product);
+                if (result >= 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMsg = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = productdal.GetProductById(id);
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)
+        {
+            try
+            {
+                int result = productdal.EditProduct(product);
+                if (result >= 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMsg = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = productdal.GetProductById(id);
+            return View(product);
+
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                int result = productdal.DeleteProduct(id);
+                if (result >= 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMsg = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ StudentCRUDUsingADO/Models/ProductDAL.cs
@@ -0,0 +1,111 @@
+﻿using System.Data.SqlClient;
+namespace StudentCRUDUsingADO.Models
+{
+    public class ProductDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        private readonly IConfiguration configuration;
+        public ProductDAL(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            string connstr = this.configuration.GetConnectionString("DefaultConnection");
+            con = new SqlConnection(connstr);
+        }
+
+
+        //Display List
+        public List<Product> GetProducts()
+        {
+            List<Product> productlist = new List<Product>();
+            string qry = "select * from product";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Product product = new Product();
+                    product.Id = Convert.ToInt32(dr["id"]);
+                    product.Name = dr["name"].ToString();
+                    product.Price = Convert.ToDouble(dr["price"]);
+                    productlist.Add(product);
+                }
+            }
+            con.Close();
+            return productlist;
+        }
+
+        //Add
+        public int AddProduct(Product product)
+        {
+            int result = 0;
+            string qry = "insert into product values(@name, @price)";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@name", product.Name);
+            cmd.Parameters.AddWithValue("@price", product.Price);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+
+        }
+
+        //Edit
+        public int EditProduct(Product product)
+        {
+            int result = 0;
+            string qry = "update product set name=@name, price=@price where id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@name", product.Name);
+            cmd.Parameters.AddWithValue("@price", product.Price);
+            cmd.Parameters.AddWithValue("@id", product.Id);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+
+        }
+
+        //Select Single Product
+
+        public Product GetProductById(int id)
+        {
+            Product product = new Product();
+            string qry = "select * from product where id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+
+                    product.Id = Convert.ToInt32(dr["id"]);
+                    product.Name = dr["name"].ToString();
+                    product.Price = Convert.ToDouble(dr["price"]);
+
+                }
+            }
+            con.Close();
+            return product;
+        }
+
+        //Delete
+
+        public int DeleteProduct(int id)
+        {
+            int result = 0;
+            string qry = "delete from product where id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+    }
+}
